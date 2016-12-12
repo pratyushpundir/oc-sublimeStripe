7ce514466a95182523570f0c3298311cd9e3aa33
Pratyush Pundir <pratyushpundir@icloud.com>
2016-12-12 14:28:28 -0600

    Add the RainLab.User depepndency back.

--- Plugin.php
+++ Plugin.php
@@ -15,7 +15,7 @@ class Plugin extends PluginBase
     /**
      * @var array Plugin dependencies
      */
-    // public $require = ['RainLab.User'];
+    public $require = ['RainLab.User'];
 
     /**
      * Returns information about this plugin.
